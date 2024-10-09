cfd1153c00ed7e8376c4d52c2cd93c3996dc983b
natalievyh <natalievhoang@gmail.com>
2024-10-09 01:40:17 -0700

    feat: create classes for tasks and projects

---
+++ src/js/classes.js
@@ -0,0 +1,40 @@
+class Task {
+    constructor (title, description, dueDate, priority) {
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority,
+        this.isCompleted = false
+    }
+
+    edit (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    toggleCompletionStatus () {
+        if (this.isCompleted) {
+            this.isCompleted = false;
+        } else {
+            this.isCompleted = true;
+        }
+    }
+}
+
+class Project {
+    constructor (name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    addTask(title, description, dueDate, priority) {
+        let task = new Task(title, description, dueDate, priority);
+        this.tasks.push(task);
+    }
+
+    removeTask(index) {
+        this.tasks.splice(index, 1);
+    }
+}
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/js/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      }
+    ],
+  },
+};
